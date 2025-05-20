fc2f73c974ee86b03938a1d506b9fe56173ba037
JF Morin <jfmorincamo@gmail.com>
2025-05-20 22:18:15 +0200

    Starting to add/try hyprlock

--- configuration.nix
+++ configuration.nix
@@ -43,20 +43,12 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  #services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  # Enable the SDDM Display Manager
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
-  # Configure keymap in X11
-  #services.xserver.xkb = {
-  #  layout = "us";
-  #  variant = "";
-  #};
+  # Enable Hyprlock
+
   
     # Flatpak
   services.flatpak.enable = true;
